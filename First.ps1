d336ed70dd3b501d8e4d8c6ef215b076d040a2ce
lbrehmerX <37737056+lbrehmerX@users.noreply.github.com>
2018-05-26 18:04:49 -0300

    Segundo Commit
    Segundo Commit

---
+++ First.ps1
@@ -0,0 +1,17 @@
+﻿<#
+.SYNOPSIS
+    Um teste simples
+.DESCRIPTION
+    List Processos e ordena
+.NOTES
+    File Name      : xxxx.ps1
+    Author         : LB
+    Prerequisite   : PowerShell V2.
+    Copyright 2011 - Free
+.LINK
+	No link
+.EXAMPLE
+    	Exemplo  1
+#>
+
+Get-Process | Sort-Object pm –desc | Select-Object –first 10
